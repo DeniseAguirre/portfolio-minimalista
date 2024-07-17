9ee34b562f34638f815974c633d4101175df8316
Denise Aguirre M <denny.aguirre01@gmail.com>
2024-07-17 16:44:23 -0300

    portfolio minimalista

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Jost", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
